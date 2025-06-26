952f963fc2df16e2a28a963c2af980441a57b297
JackAtVisir <jack.trevaskis@visir.ai>
2025-06-26 19:50:41 +1200

    save game

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,10 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
+  Game: a
     .model({
-      content: a.string(),
+      gameScore: a.integer(),
+      upgrade: a.integer()
     })
     .authorization((allow) => [allow.owner()]),
 });
--- src/main.tsx
+++ src/main.tsx
@@ -8,25 +8,31 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import App1 from "./App1.tsx";
+
 Amplify.configure(outputs);
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: (
-      <Authenticator>
         <App />
-      </Authenticator>
     ),
   },
   {
     path: "/cookie",
     element: <Cookie />,
   },
+  {
+    path: "/rah",
+    element: <App1 />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Authenticator>
+      <RouterProvider router={router} />
+    </Authenticator>
   </React.StrictMode>
 );
